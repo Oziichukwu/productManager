7748745b992a2eb3cb5520bcc66e9f82209c788e
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-10-27 09:13:12 +0100

    web security

--- src/main/java/com/example/productManager/config/UserDetailsServiceImpl.java
+++ src/main/java/com/example/productManager/config/UserDetailsServiceImpl.java
@@ -1,2 +1,24 @@
-package com.example.productManager.config;public class UserDetailsServiceImpl {
+package com.example.productManager.config;
+
+import com.example.productManager.data.models.ProductUser;
+import com.example.productManager.data.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        ProductUser user = userRepository.getUserByUsername(username);
+        if(user == null){
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new MyUserDetails(user);
+    }
 }
