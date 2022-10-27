b770f883eb2890abbb1d9f3e4f55aaef2f168664
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-10-27 09:14:12 +0100

    product user and product repository added

--- src/main/java/com/example/productManager/data/models/ProductUser.java
+++ src/main/java/com/example/productManager/data/models/ProductUser.java
@@ -1,2 +1,25 @@
-package com.example.productManager.data.models;public class ProductUser {
+package com.example.productManager.data.models;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "users")
+public class ProductUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String username;
+
+    private String role;
+
+    private String password;
+
+    private boolean enabled;
 }
--- src/main/java/com/example/productManager/data/repositories/UserRepository.java
+++ src/main/java/com/example/productManager/data/repositories/UserRepository.java
@@ -1,2 +1,13 @@
-package com.example.productManager.data.repositories;public interface UserRepository {
+package com.example.productManager.data.repositories;
+
+import com.example.productManager.data.models.ProductUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends CrudRepository<ProductUser, Long> {
+
+    @Query("SELECT u FROM ProductUser u WHERE u.username = :username")
+    public ProductUser getUserByUsername(@Param("username") String username);
 }
